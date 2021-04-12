af26199a953f3d3b405d4d74dde07b949237b875
Nerinsky <neri685@gmail.com>
2021-04-12 18:36:54 -0500

    Creacion Screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  return (
+return (
+  <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -11,52 +14,17 @@ function App() {
           </a>
         </div>
         <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+          <a href="/cart">Carrito</a>
+          <a href="/signin">Registrase</a>
         </div>
       </header>
       <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
       </main>
       <footer className="row center">ITGAM-GIMEX-NERI All right reserved</footer>
     </div>
+  </BrowserRouter>
   );
 }
 
