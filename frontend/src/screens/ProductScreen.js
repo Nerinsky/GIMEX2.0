a026b20428daf36cc422babd2fb906ccdd8d8d61
Nerinsky <neri685@gmail.com>
2021-04-15 22:50:45 -0500

    Agregando Redux a las Screens

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,17 +1,30 @@
 import Rating from "../components/Rating";
-import data from "../data";
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { detailsProduct } from "../actions/productActions";
 
 export default function ProductScreen(props) 
 {
-    const product = data.products.find((x) => x._id === props.match.params.id);
-        if (!product) 
-        {
-            return <div> Product Not Found</div>;
-        }
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector((state) => state.productDetails);
+    const { loading, error, product} = productDetails;
+
+    useEffect(() =>
+    {
+        dispatch(detailsProduct(productId));
+    }, [dispatch, productId]);
+
     return (
         <div>
+            {loading? (<LoadingBox></LoadingBox>)
+            :
+            error? (<MessageBox variant="danger">{error}</MessageBox>)
+            :(
+<div>
             <Link to="/">Regresar Inicio</Link>
             <div className="row top">
                 <div className="col-2">
@@ -66,5 +79,10 @@ export default function ProductScreen(props)
                 </div>
             </div>
         </div>
+            )}
+        </div>
+
+
+        
     );
 }
